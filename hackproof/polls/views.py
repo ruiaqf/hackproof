941ed2e8505d7d8d8f47b9b2404cc4a8261988f2
Rui Quental <ruiaqf@gmail.com>
2024-05-22 17:20:19 +0100

    some additions

--- hackproof/polls/views.py
+++ hackproof/polls/views.py
@@ -10,3 +10,27 @@ def login(request):
 
 def register(request):
     return render(request, "register.html")
+
+def tables(request):
+    return render(request, "tables.html")
+
+def dicas(request):
+    return render(request, "dicas.html")
+
+def password(request):
+    return render(request, "password.html")
+
+def charts(request):
+    return render(request, "charts.html")
+
+def dicas(request):
+    return render(request, "dicas.html")
+
+def page_401(request):
+    return render(request, "401.html")
+
+def page_404(request):
+    return render(request, "404.html")
+
+def page_500(request):
+    return render(request, "500.html")

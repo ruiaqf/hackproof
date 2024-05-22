dde87cc563b10794f345f296ae9fdd0e697ab28c
Rui Quental <ruiaqf@gmail.com>
2024-05-22 15:58:53 +0100

    index a funcionar

--- hackproof/polls/urls.py
+++ hackproof/polls/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
 ]
